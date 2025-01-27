2efd7116d160fc6d98a407d07d76f82ad297b6a3
vaishnavi gadhe <gadhevaishnavi70@gmail.com>
2025-01-27 14:07:56 +0530

    add image section

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../assets/css/Home.css'; // Ensure you have the appropriate CSS for styling
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS for styles
-import img1 from '../assets/images/img1.jpeg'
-import img2 from '../assets/images/img2.jpeg'
-import img3 from '../assets/images/img3.jpeg'
+import img1 from '../assets/images/sliderimg1.webp'
+import img2 from '../assets/images/sliderimg22.avif'
+import img3 from '../assets/images/sliderimg3.webp'
 import '../assets/css/Home.css'
 import Carousel from 'react-bootstrap/Carousel';
 import AboutUs from './AboutUs';
